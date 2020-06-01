d45b2efdaf2a852fa887abcc022a0c9d306f4d16
PugnaBogdan <bpugna@gmail.com>
2020-06-02 00:22:55 +0300

    participants

--- frontend/frontISS/src/app/participant/participant.component.ts
+++ frontend/frontISS/src/app/participant/participant.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Paper } from '../models/paper';
+import { Conference } from '../models/conference';
+import { Program } from '../models/Program';
+import { Location } from '../models/Location';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConferenceChairServiceService } from '../Shared/conference-chair-service.service';
 
 @Component({
   selector: 'app-participant',
@@ -8,10 +13,34 @@ import { Paper } from '../models/paper';
 })
 export class ParticipantComponent implements OnInit {
   paper:Paper;
-  constructor() { }
-
-  ngOnInit() {
-    this.paper= new Paper(1, 'a', 'b','c','b','d','v');
+  conference: Conference;
+  location: Location;
+  program: Program;
+  errorMessage: string;
+  name:string;
+  constructor(
+    private route: ActivatedRoute,
+              private router: Router,
+              private service: ConferenceChairServiceService
+  ) { 
+    this.location = new Location(null,null,null,null);
+    this.program = new Program(null,null,null,null,null,null);
+    this.conference = new Conference(null,null,this.location,null,this.program,null,null,null);
+    this.name = localStorage.getItem('email');
   }
 
+  ngOnInit() {
+    //this.paper= new Paper(1, 'a', 'b','c','b','d','v');
+    this.getConference();
+
+
+  }
+
+  getConference() {
+    this.service.getConference(1)
+      .subscribe(
+        conference => this.conference = conference,
+        error => this.errorMessage = <any>error
+      );
+  }
 }
