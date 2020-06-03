3fdeb7831647f2a28ec442cb41f7e29f0970f21f
Stefan Radu <radustefan29@yahoo.com>
2020-06-03 17:43:14 +0300

    added participant to index.html

--- backend/src/main/java/com/config/JPAConfig.java
+++ backend/src/main/java/com/config/JPAConfig.java
@@ -31,9 +31,9 @@ public class JPAConfig {
         config.setValidationTimeout(TimeUnit.SECONDS.toMillis(2));
         config.setDriverClassName(org.postgresql.Driver.class.getName());
 
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/i");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/isstest");
         config.setUsername("postgres");
-        config.setPassword("gloria99");
+        config.setPassword("parola");
 
         return new HikariDataSource(config);
     }
